a055fa7de36f9756ceec2c484da6a84692e1962b
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-06-22 21:31:17 +0200

    declare main method

--- src/main/java/com/sverdiyev/tracker/MainVerticle.java
+++ src/main/java/com/sverdiyev/tracker/MainVerticle.java
@@ -2,8 +2,19 @@ package com.sverdiyev.tracker;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
 
 public class MainVerticle extends AbstractVerticle {
+
+  public static void main(String[] args) {
+    var vertx = Vertx.vertx();
+
+    vertx.deployVerticle(new MainVerticle(), result -> {
+      if (result.failed()) {
+        System.out.println("Failed to deploy");
+      }
+    });
+  }
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
