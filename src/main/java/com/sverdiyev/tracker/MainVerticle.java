fcfdb0564fd59510bc71e0d9f4d08fef853641d2
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-06-22 21:45:01 +0200

    add router and restApi

--- src/main/java/com/sverdiyev/tracker/MainVerticle.java
+++ src/main/java/com/sverdiyev/tracker/MainVerticle.java
@@ -3,6 +3,8 @@ package com.sverdiyev.tracker;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -17,18 +19,31 @@ public class MainVerticle extends AbstractVerticle {
   }
 
   @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
+  public void start(Promise<Void> startPromise) {
+    var server = vertx.createHttpServer();
+
+    Router restApi = Router.router(vertx);
+    restApi.get("/test").handler(ctx -> {
+
+      JsonObject res = new JsonObject();
+
+      res.put("hello", "world");
+
+      ctx.response().end(res.toBuffer());
     });
+
+    server.requestHandler(restApi).listen(8888);
+
+//    vertx.
+//      .requestHandler(req -> req.response()
+//        .putHeader("content-type", "text/plain")
+//        .end("Hello from Vert.x!")).listen(8888, http -> {
+//        if (http.succeeded()) {
+//          startPromise.complete();
+//          System.out.println("HTTP server started on port 8888");
+//        } else {
+//          startPromise.fail(http.cause());
+//        }
+//      });
   }
 }
