3bec733d863006d78419d0ec01a421fe0e9f365e
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-06-23 17:34:06 +0200

    add in-memory cache and error http request

--- src/main/java/com/sverdiyev/tracker/routers/QuotesRestApi.java
+++ src/main/java/com/sverdiyev/tracker/routers/QuotesRestApi.java
@@ -2,9 +2,14 @@ package com.sverdiyev.tracker.routers;
 
 import com.sverdiyev.tracker.models.Quote;
 import com.sverdiyev.tracker.models.Stock;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
 
@@ -17,12 +22,20 @@ public class QuotesRestApi {
     parent.get("/quotes/:stockName").handler(ctx -> {
 
       var stockName = ctx.pathParam("stockName");
+      var cachedQuotes = getCachedQuotes();
 
-      var res = randomQuote(stockName);
+      var cachedQuote = cachedQuotes.get(stockName);
+      if (cachedQuote != null) {
+        var arr = new JsonArray();
+        arr.add(cachedQuotes.get(stockName));
+        ctx.response().end(arr.toBuffer());
+      } else {
 
-      var arr = new JsonArray();
-      arr.add(res);
-      ctx.response().end(arr.toBuffer());
+        var res = (new JsonObject());
+
+        res.put("message", "No Quote was found");
+        ctx.response().setStatusCode(HttpResponseStatus.NOT_FOUND.code()).end(res.toBuffer());
+      }
     });
   }
 
@@ -34,6 +47,20 @@ public class QuotesRestApi {
       .volume(randomValue())
       .stock(Stock.builder().ticker(stockName).build())
       .build();
+  }
+
+
+  private static Map<String, Quote> getCachedQuotes() {
+
+    HashMap<String, Quote> cachedQuotes = new HashMap<>();
+
+    List<String> symbols = List.of("AAPL", "SP500", "GGL", "NTFLX");
+
+    for (String symbol : symbols) {
+      cachedQuotes.put(symbol, randomQuote(symbol));
+    }
+
+    return cachedQuotes;
   }
 
 
